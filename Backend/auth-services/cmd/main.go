d3e95ca32f75282907548a9930035ca0cf901b42
T042JasonKennethLay <jasonkenneth2807@gmail.com>
2026-07-09 13:36:32 +0700

    yang udah di megre sama main go

--- Backend/auth-services/cmd/main.go
+++ Backend/auth-services/cmd/main.go
@@ -3,12 +3,11 @@ package cmd
 import (
 	"fmt"
 	"log"
-	"net/smtp"
 	"strconv"
 	"time"
 
 	"github.com/Acad600-TPA/WEB-MT-AO-KY-ON-CJ-261/auth-service/repository"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"honnef.co/go/tools/config"
 )
@@ -18,37 +17,36 @@ func main() {
 
 	smtpPortInt, err := strconv.Atoi(config.SMTPPort)
 
-	if err != nil{
+	if err != nil {
 		log.Fatalf("failed to convert SMPT_PORT to int: %s", err)
 	}
 
 	db, err := gorm.Open(postgres.Open(config.DBString))
 
-
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
 		config.DBHost, config.DBUser, config.DBPassword, config.DBName, config.DBPort)
 	db, err = gorm.Open(postgres.Open(dsn))
 
-	if err != nil{
+	if err != nil {
 		log.Fatalf("failed to run migration: %s", err)
 	}
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:	config.RedisAddr,
-		DialTimeout: 10 * time.Second,
-		ReadTimeout: 30 * time.Second,
+		Addr:         config.RedisAddr,
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
-		PoolSize: 10,
-		PoolTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
 	})
-	
+
 	clusterClient := redis.NewClusterClient(&redis.ClusterOptions{
-		Addr:	string.Split(config.RedisAddr, ","),
-		DialTimeout: 10 * time.Second,
-		ReadTimeout: 30 * time.Second,
+		Addr:         string.Split(config.RedisAddr, ","),
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
-		PoolSize: 10,
-		PoolTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
 	})
 
 	userRepo := repository.NewUserRepo(db)
@@ -57,5 +55,5 @@ func main() {
 	refreshStore := cache.NewRedisRefreshStore(clusterClient)
 
 	authUseCase := usecase.NewAuthUseCase(userRepo, otpRepo, tokenBlacklist, refreshStore, config)
-	authHandler := http.NewAuthHandler (autUseCase)
+	authHandler := http.NewAuthHandler(autUseCase)
 }
