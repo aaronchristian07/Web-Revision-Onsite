b70bdc13054909039ad5a9066dee782961ced993
T042JasonKennethLay <jasonkenneth2807@gmail.com>
2026-07-09 12:04:31 +0700

    update dto dan model

---
+++ Backend/auth-services/model/auth_model.go
@@ -0,0 +1,16 @@
+package model
+
+type User struct {
+	ID       string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Username string `gorm:"not null"`
+	Email    string `gorm:"uniqueIndex;not null"`
+	Password string `gorm:"not null"`
+	Role     string `gorm:"not null"`
+}
+
+type RefreshToken struct {
+	ID     string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserID string `gorm:"uniqueIndex;not null"`
+	Email  string `gorm:"uniqueIndex;not null"`
+	Role   string `gorm:"not null"`
+}
